d05f9a61449d967193c233c7a62ee009f7068cc0
= <=>
2013-02-19 09:36:21 -0800

    initialized a rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InTheMorning::Application.config.secret_token = '4022c929eb76cb88b095e302393c7732a5200b19d7dc5073c4020ece22277e789cb573a83ad1463c164cac2ef09d9c8a311bbc7c7d313ebeeb69782117640724'
